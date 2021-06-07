5c61562459c251fae956502b48500f4501286936
Sundar Urs <sundarkurs@gmail.com>
2021-06-07 21:15:54 +0530

    added spinner to show loading

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -4,11 +4,14 @@ import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import { MEALS_DATA as MealsTempData } from "../../common/MealsData";
 import axios from "../../context/FoodDbContext";
+import Spinner from "../UI/Spinner";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("/meals.json")
       .then((response) => {
@@ -23,16 +26,26 @@ const AvailableMeals = () => {
           });
         }
         setMeals(mealsList);
+        setIsLoading(false);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        setIsLoading(false);
+      });
   }, []);
+
+  let content = "";
+
+  if (isLoading) {
+    content = <Spinner></Spinner>;
+  } else if (!isLoading && meals.length <= 0) {
+    content = <p>Found no meals.</p>;
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
+        {content}
         <ul>
-          {meals.length <= 0 && <p>Found no meals.</p>}
-
           {meals.map((meal) => (
             <MealItem key={meal.id} item={meal}></MealItem>
           ))}
