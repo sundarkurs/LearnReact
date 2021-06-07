778ff076cb4413cf606d6300778a5feda12a24d4
Sundar Urs <sundarkurs@gmail.com>
2021-06-07 21:01:44 +0530

    added axios to get data from the firebase

--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 import { MEALS_DATA as MealsTempData } from "../../common/MealsData";
+import axios from "../../context/FoodDbContext";
 
 const AvailableMeals = () => {
-  const mealsList = MealsTempData;
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/meals.json")
+      .then((response) => {
+        console.log("Meals retrieved.");
+        const mealsList = [];
+        for (const key in response.data) {
+          mealsList.push({
+            id: key,
+            name: response.data[key].name,
+            description: response.data[key].description,
+            price: response.data[key].price,
+          });
+        }
+        setMeals(mealsList);
+      })
+      .catch((error) => {});
+  }, []);
 
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {mealsList.map((meal) => (
+          {meals.length <= 0 && <p>Found no meals.</p>}
+
+          {meals.map((meal) => (
             <MealItem key={meal.id} item={meal}></MealItem>
           ))}
         </ul>
