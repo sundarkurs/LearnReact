2cf775d45041b2d0b3b42dcdc40697c18b4a0dc0
Sundar Urs <sundarkurs@gmail.com>
2021-06-11 16:03:40 +0530

    done with routing

--- route-quotes-app/src/components/comments/NewCommentForm.js
+++ route-quotes-app/src/components/comments/NewCommentForm.js
@@ -1,26 +1,43 @@
-import { useRef } from 'react';
-
-import classes from './NewCommentForm.module.css';
+import { useEffect, useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import classes from "./NewCommentForm.module.css";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
+  const { sendRequest, status, error } = useHttp(addComment, false);
+
+  const { onAddedComment } = props;
+  useEffect(() => {
+    if ((status === "completed") & !error) {
+      commentTextRef.current.value = "";
+      onAddedComment();
+    }
+  }, [status, error]);
+
   const submitFormHandler = (event) => {
     event.preventDefault();
 
-    // optional: Could validate here
+    const enteredText = commentTextRef.current.value;
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner></LoadingSpinner>
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
--- route-quotes-app/src/lib/api.js
+++ route-quotes-app/src/lib/api.js
@@ -56,7 +56,7 @@ export async function addQuote(quoteData) {
 
 export async function addComment(requestData) {
   const response = await fetch(
-    `${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`,
+    `${FIREBASE_DOMAIN}/quote-comments/${requestData.quoteId}.json`,
     {
       method: "POST",
       body: JSON.stringify(requestData.commentData),
@@ -75,10 +75,9 @@ export async function addComment(requestData) {
 }
 
 export async function getAllComments(quoteId) {
-  const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`);
+  const response = await fetch(`${FIREBASE_DOMAIN}/quote-comments/${quoteId}.json`);
 
   const data = await response.json();
-
   if (!response.ok) {
     throw new Error(data.message || "Could not get comments.");
   }
