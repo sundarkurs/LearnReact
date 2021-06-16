5be7bd3f1e7ddd0a1d9a8fb71f6f41b3b5a8b865
Sundar Urs <sundarkurs@gmail.com>
2021-06-16 10:36:27 +0530

    some more refactoring

--- material-ui-sample/src/App.js
+++ material-ui-sample/src/App.js
@@ -1,4 +1,5 @@
 import { Fragment, useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import MainLayout from "./components/Layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import Types from "./pages/Types";
@@ -9,8 +10,20 @@ const App = () => {
   return (
     <Fragment>
       <MainLayout pageTitle={pageTitle}>
-        <Types></Types>
-        <Dashboard></Dashboard>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="dashboard"></Redirect>
+          </Route>
+          <Route path="/dashboard" exact>
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/asset-types" exact>
+            <Types></Types>
+          </Route>
+          <Route path="*">
+            <p>Not found</p>
+          </Route>
+        </Switch>
       </MainLayout>
     </Fragment>
   );
--- material-ui-sample/src/components/Layout/Menu/MenuItems.js
+++ material-ui-sample/src/components/Layout/Menu/MenuItems.js
@@ -3,39 +3,55 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
-import DashboardIcon from "@material-ui/icons/Dashboard";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+import AppsIcon from "@material-ui/icons/Apps";
+import CollectionsIcon from "@material-ui/icons/Collections";
 import PeopleIcon from "@material-ui/icons/People";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import LayersIcon from "@material-ui/icons/Layers";
 import AssignmentIcon from "@material-ui/icons/Assignment";
+import { NavLink } from "react-router-dom";
 
 export const primaryMenuItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItem>
+    <NavLink
+      to="/dashboard"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItem button>
+        <ListItemIcon>
+          <AppsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </NavLink>
+
+    <NavLink
+      to="/asset-types"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItem button>
+        <ListItemIcon>
+          <CollectionsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Types" />
+      </ListItem>
+    </NavLink>
+
     <ListItem button>
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
       <ListItemText primary="Customers" />
     </ListItem>
+
     <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
       <ListItemText primary="Reports" />
     </ListItem>
+
     <ListItem button>
       <ListItemIcon>
         <LayersIcon />
