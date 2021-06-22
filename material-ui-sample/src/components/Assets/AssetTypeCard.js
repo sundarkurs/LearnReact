bb3ee5dd70160a7f1fe47aedab925cc3eec75c7e
Sundar Urs <sundarkurs@gmail.com>
2021-06-22 21:06:06 +0530

    asset type card

--- material-ui-sample/src/components/Assets/AssetTypeCard.js
+++ material-ui-sample/src/components/Assets/AssetTypeCard.js
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-
 import folderImage from "../../assets/folder-image.png";
+import Avatar from "@material-ui/core/Avatar";
+import CardHeader from "@material-ui/core/CardHeader";
+import { red } from "@material-ui/core/colors";
+import ImageTypeImage from "../../assets/asset-type-images/image-type.jpeg";
+import ServiceDocumentImage from "../../assets/asset-type-images/service-document.jpg";
+import UserManualImage from "../../assets/asset-type-images/users-manual.jpg";
+import ProductImage from "../../assets/asset-type-images/product-image.png";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: 345,
-    height: 250,
+    maxWidth: 345,
   },
-});
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 export default function AssetTypeCard(props) {
   const classes = useStyles();
 
+  const getAvatarText = (name) => {
+    var matches = name.match(/\b(\w)/g);
+    var acronym = matches.join("");
+    return acronym;
+  };
+
   return (
-    <Card className={classes.root} onClick={() => props.onAssetTypeClick(props.assetType)}>
+    <Card onClick={() => props.onAssetTypeClick(props.assetType)}>
       <CardActionArea>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              {getAvatarText(props.assetType.name)}
+            </Avatar>
+          }
+          title={
+            <Typography variant="h5" color="textSecondary" component="h2">
+              {props.assetType.name}
+            </Typography>
+          }
+          subheader={props.assetType.code}
+        />
+
         <CardMedia
           component="img"
           alt={props.assetType.name}
           height="140"
-          image={folderImage}
+          image={ServiceDocumentImage}
           title={props.assetType.name}
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.assetType.name}
-          </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             {props.assetType.description}
           </Typography>
